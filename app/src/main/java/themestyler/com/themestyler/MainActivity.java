0dbb94088d734f319ad1cc8bd19a57c54df932a4
Smarakjeet <smarak.jeetnayak301@gmail.com>
2019-02-05 10:14:51 +0530

    Toast with Image test

--- app/src/main/java/themestyler/com/themestyler/MainActivity.java
+++ app/src/main/java/themestyler/com/themestyler/MainActivity.java
@@ -3,11 +3,16 @@ package themestyler.com.themestyler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import themestyler.com.customtoast.CustomToast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CustomToast.s(this,"Hello Bro");
+
     }
 }
--- customtoast/src/main/java/themestyler/com/customtoast/CustomToast.java
+++ customtoast/src/main/java/themestyler/com/customtoast/CustomToast.java
@@ -1,13 +1,14 @@
 package themestyler.com.customtoast;
 
 import android.content.Context;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class CustomToast {
 
-    public static void s(Context c, String Message){
+    public static void s(Context c, String Message, ImageView id){
 
-        Toast.makeText(c,Message,Toast.LENGTH_SHORT).show();
+        Toast.makeText(c,Message+""+id,Toast.LENGTH_SHORT).show();
 
     }
 }
